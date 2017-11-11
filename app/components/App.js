2c542b5e79e922b2778c6dfa656dd7d1bd2892e1
Joshua Huffman <j.m.huffman@gmail.com>
2017-11-11 11:41:57 -0800

    add routing with react-router-dom

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,29 @@
 var React = require('react');
 var Popular = require('./Popular');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
+var Switch = ReactRouter.Switch;
+
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <Popular />
-            </div>
+            <Router>
+                <div className='container'>
+                    <Nav/>
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                        <Route exact path='/battle' component={Battle} />
+                        <Route path='/popular' component={Popular} />
+                        <Route render={function() {
+                            return <p>Not Found</p>
+                        }} />
+                    </Switch>
+                </div>
+            </Router>
         )
     }
 }
