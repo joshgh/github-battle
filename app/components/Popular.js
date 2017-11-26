cae88cb0bdf4e1dec71133ec6feef300f1d3e7b0
Joshua Huffman <j.m.huffman@gmail.com>
2017-11-25 18:56:10 -0800

    add default props to loading component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var api = require('../utils/api.js');
+var Loading = require('./Loading');
 
 function SelectLanguage (props) {
   var languages = ['All', 'JavaScript', 'Ruby', 'PHP', 'CSS', 'Python'];
@@ -89,7 +90,7 @@ class Popular extends React.Component {
           selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage}/>
         {!this.state.repos ?
-          <p>Loading</p> :
+          <Loading text='Downloading' speed={10}/> :
           <RepoGrid repos={this.state.repos} />
         }
 
