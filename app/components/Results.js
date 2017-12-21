c7d667ae9151018e746a2c9e2e28084b930c0ac7
Joshua Huffman <j.m.huffman@gmail.com>
2017-12-20 17:34:16 -0800

    refactor Results.js to use es6 syntax

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,14 +1,12 @@
-var React = require('react');
-var PropTypes = require('prop-types');
-var queryString = require('query-string');
-var api = require('../utils/api');
-var Link = require('react-router-dom').Link;
-var PlayerPreview = require('./PlayerPreview');
-var Loading = require('./Loading');
+const React = require('react');
+const PropTypes = require('prop-types');
+const queryString = require('query-string');
+const api = require('../utils/api');
+const Link = require('react-router-dom').Link;
+const PlayerPreview = require('./PlayerPreview');
+const Loading = require('./Loading');
 
-var Profile = function(props) {
-    var info = props.info;
-    return (
+const Profile = ({info}) => (
         <PlayerPreview
             avatar={info.avatar_url}
             username={info.login}
@@ -23,22 +21,19 @@ var Profile = function(props) {
                 {info.blog && <li><a href={info.blog}>{info.blog}</a></li>}
             </ul>
         </PlayerPreview>
-    )
-}
+    );
 
 Profile.propTypes = {
     info: PropTypes.object.isRequired,
 }
 
-var Player = function(props) {
-    return (
+const Player = ({ label, score, profile}) => (
         <div>
-            <h1 className='header'>{props.label}</h1>
-            <h3 style={{textAlign: 'center'}}>Score: {props.score}</h3>
-            <Profile info={props.profile}/>
+            <h1 className='header'>{label}</h1>
+            <h3 style={{textAlign: 'center'}}>Score: {score}</h3>
+            <Profile info={profile}/>
         </div>
-    )
-}
+    );
 
 Player.propTypes = {
     label: PropTypes.string.isRequired,
@@ -58,35 +53,29 @@ class Results extends React.Component {
         }
     }
     componentDidMount () {
-        var players = queryString.parse(this.props.location.search);
+        const { playerOneName, playerTwoName } = queryString.parse(this.props.location.search);
         api.battle([
-            players.playerOneName,
-            players.playerTwoName
-        ]).then(function (results) {
+            playerOneName,
+            playerTwoName
+        ]).then((results) => {
             if(null === results) {
-                return this.setState(function () {
-                    return {
+                return this.setState(() => ({
                         error: 'Looks like there was error. Check that both users exist on Github',
                         loading: false,
-                    }
-                })
+                }));
             }
 
-            this.setState(function (){
-                return {
+            this.setState(() => ({
                     error: null,
                     winner: results[0],
                     loser: results[1],
                     loading: false
                 }
-            })
-        }.bind(this));
+            ));
+        });
     }
     render() {
-        var error = this.state.error;
-        var winner = this.state.winner;
-        var loser = this.state.loser;
-        var loading = this.state.loading;
+        const { error, winner, loser, loading } = this.state;
 
         if (true === loading) {
             return <Loading />
